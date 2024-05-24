0e027066a4e955fc341b8aea4e9c0fae5a4426fb
Kolli Chaitanya <venkatachaitanya095@gmail.com>
2024-05-24 15:37:07 +0530

    Added the log function to weather app #4

--- Source/WeatherApp/flask_auth/routes.py
+++ Source/WeatherApp/flask_auth/routes.py
@@ -12,9 +12,19 @@ import os
 import warnings
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+import logging
+from logging.handlers import RotatingFileHandler
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler = RotatingFileHandler('app.log', maxBytes=1024 * 1024 * 10, backupCount=5)
+file_handler.setFormatter(formatter)
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.DEBUG)
+
 
 GEODB_API_KEY = '03b7c4734dmsha8ae33637250f48p11c63fjsn48372cbe71f6'
 def get_cities():
+
     url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
     headers = {
         "X-RapidAPI-Key": GEODB_API_KEY,
@@ -29,11 +39,13 @@ def tocelcius(temp):
     return str(round(float(temp) - 273.16,2))
 @app.route('/')
 def home():
+    app.logger.info('Loading the home page')
     return render_template('home.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    app.logger.info('Loading the register page')
     form = RegistrationForm()
     if form.validate_on_submit():
         username = form.username.data
@@ -46,6 +58,7 @@ def register():
         cursor.close()
 
         flash('You have successfully registered!', 'success')
+        app.logger.info(f'user:{username} account has been created')
         return redirect(url_for('login'))
 
     return render_template('register.html', form=form)
@@ -53,6 +66,7 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    app.logger.info('Loading the login page')
     form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data
@@ -66,8 +80,10 @@ def login():
         if user and bcrypt.check_password_hash(user[3], password):  # user[3] is the password field
             session['username'] = user[1]  # user[1] is the username field
             flash('Login successful!', 'success')
+            app.logger.info(f'user:{username} has logged in successfully')
             return redirect(url_for('dashboard'))
         else:
+            app.logger.warning('Wrong credentials have been used for login')
             flash('Login Unsuccessful. Please check username and password', 'danger')
 
     return render_template('login.html', form=form)
@@ -75,17 +91,14 @@ def login():
 
 @app.route('/dashboard')
 def dashboard():
-    #if 'username' not in session:
-    #    flash('You are not logged in!', 'warning')
-    #    return redirect(url_for('login'))
-
-    #return f'Welcome to your dashboard, {session["username"]}! <a href="/logout">Logout</a>'
+    app.logger.info('Loading the dashboard')
     if 'username' in session:
         return render_template('dashboard.html')
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
+    app.logger.info(f'the user has logged out')
     flash('You have been logged out.', 'info')
     return redirect(url_for('login'))
 
@@ -93,14 +106,16 @@ def logout():
 
 @app.route('/page1',methods=['POST','GET'])
 def weather():
+    app.logger.info('Loading the weather search page')
     api_key = '48a90ac42caa09f90dcaeee4096b9e53'
     if request.method == 'POST':
         city = request.form['city']
     else:
-        #for default name mathura
         city = 'mathura'
-
-    # source contain json data from api
+    if not city:
+        app.logger.warning('No city has been provided for the search')
+    else:
+        app.logger.info(f'The city : {city} was given for the search')
     try:
         source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid='+api_key).read()
     except:
@@ -120,38 +135,7 @@ def weather():
         "cityname":str(city),
     }
     return render_template('page1.html',data=data)
-#@app.route('/page2')
-#def page2():
-#    return render_template('page2.html')
-@app.route('/page2',methods=['POST','GET'])
-def weather1():
-    api_key = '48a90ac42caa09f90dcaeee4096b9e53'
-    if request.method == 'POST':
-        city = request.form['city']
-    else:
-        #for default name mathura
-        city = 'mathura'
 
-    # source contain json data from api
-    try:
-        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid='+api_key).read()
-    except:
-        return abort(404)
-    # converting json data to dictionary
-
-    list_of_data = json.loads(source)
-
-    # data for variable list_of_data
-    data = {
-        "country_code": str(list_of_data['sys']['country']),
-        "coordinate": str(list_of_data['coord']['lon']) + ' ' + str(list_of_data['coord']['lat']),
-        "temp": str(list_of_data['main']['temp']) + 'k',
-        "temp_cel": tocelcius(list_of_data['main']['temp']) + 'C',
-        "pressure": str(list_of_data['main']['pressure']),
-        "humidity": str(list_of_data['main']['humidity']),
-        "cityname":str(city),
-    }
-    return render_template('page2.html',data=data)
 @app.route('/predict',methods=['POST','GET'])
 def predict():
     if not os.path.isfile('model.pkl'):
