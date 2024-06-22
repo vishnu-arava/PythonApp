2e9c3857b70c66901dc9cf6754793720a986057c
Kolli Chaitanya <venkatachaitanya095@gmail.com>
2024-06-22 20:05:09 +0530

    ODBC drivers for ubuntu

--- Source/WeatherApp/run.py
+++ Source/WeatherApp/run.py
@@ -3,24 +3,7 @@ import sys
 import subprocess
 import os
 
-def install_python3_venv():
-   try:
-       subprocess.check_call(['sudo', 'apt-get', 'update'])
-       subprocess.check_call(['sudo', 'apt-get', 'install', '-y', 'python3*-venv'])
-       print("python3.10-venv installed successfully.")
-   except subprocess.CalledProcessError as e:
-       print(f"An error occurred while installing python3.10-venv: {e}")
-       raise
-
-def create_virtualenv(venv_path):
-    try:
-
-        subprocess.check_call([sys.executable, '-m', 'venv', venv_path])
-        print(f"Virtual environment created at {venv_path}.")
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred while creating the virtual environment: {e}")
-        raise
-def install_packages(venv_path, requirements_file):
+def install_packages():
     try:
         pip_executable = os.path.join(venv_path, 'bin', 'pip') if os.name != 'nt' else os.path.join(venv_path, 'Scripts', 'pip')
         odbc17  = os.path.abspath('odbc17.sh')
@@ -31,14 +14,14 @@ def install_packages(venv_path, requirements_file):
         subprocess.check_call([pip_executable, 'install', 'pipreqs'])
         print("pipreqs installed successfully.")
 
-        subprocess.run([os.path.join(venv_path, 'bin', 'pipreqs'), os.path.abspath(os.getcwd())])
-
-        subprocess.check_call(['sudo', 'apt-get', 'install', '-y', 'pkg-config'])
-        subprocess.check_call(['sudo', 'apt-get', 'install', '-y', 'libmysqlclient-dev'])
+        subprocess.run(['pipreqs', '.'])
+        subprocess.run(['sed','-i','/azure_storage/d', 'requirements.txt'])      
+        # subprocess.check_call(['sudo', 'apt-get', 'install', '-y', 'pkg-config'])
+        # subprocess.check_call(['sudo', 'apt-get', 'install', '-y', 'libmysqlclient-dev'])
         subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
         subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'setuptools'])
-
-        subprocess.check_call([sys.executable, '-m','pip','install','-r',requirements_file])
+        
+        subprocess.check_call([sys.executable, '-m','pip','install','-r','requirements.txt'])
         print("Required packages installed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while installing packages: {e}")
@@ -46,11 +29,7 @@ def install_packages(venv_path, requirements_file):
 
 if __name__ == '__main__':
     if (os.name=='posix'):
-        venv_path = "./venv"
-        requirements_file = 'requirements.txt'
-        install_python3_venv()
-        create_virtualenv(venv_path)
-        install_packages(venv_path, requirements_file)
+        install_packages()
     if (os.name=='nt'):
         venv_path = "venv"
         subprocess.run([sys.executable, "-m", "venv", venv_path])
