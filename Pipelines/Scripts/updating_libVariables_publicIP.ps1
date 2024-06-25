3fb522141fc32f5200841edf2cfeaa269cd5ac9c
Kolli Chaitanya <venkatachaitanya095@gmail.com>
2024-06-25 17:11:01 +0530

    Created PowerShell Script for Library Variable update #17

---
+++ Pipelines/Scripts/updating_libVariables_publicIP.ps1
@@ -0,0 +1,42 @@
+Param(
+    [string]$organizationName,  # Your Azure DevOps organization name
+    [string]$projectName,       # Your project name
+    [string]$libVarGroupId,     # The ID of the variable group
+    [string]$libVariableName,   # The name of the variable to update
+    [string]$patToken           # Personal Access Token (PAT)
+)
+
+
+
+$var = ConvertFrom-Json '$(hostname)'
+$value = $var.hostname.value
+$publicIpValue = $value
+
+# Base64 encode the PAT
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$patToken"))
+
+# Get the existing variable group
+$uri = "https://dev.azure.com/$organizationName/$projectName/_apis/distributedtask/variablegroups/$libVarGroupId"+"?api-version=6.0-preview.2"
+
+Write-Host "Url is : $uri"
+try {
+    $response = Invoke-RestMethod -Uri $uri -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
+
+    # Update the variable value
+    $response.variables.$libVariableName.value = $publicIpValue
+
+    # Update the variable group
+    $json = $response | ConvertTo-Json -Depth 100
+    Invoke-RestMethod -Uri $uri -Method Put -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo); "Content-Type"="application/json"} -Body $json
+    Write-Host "Variable group updated successfully with public IP: $publicIpValue"
+    exit 0
+}
+catch {
+    Write-Host "Variable group updated un-successfully"
+    "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+    exit 1
+}
+
+
+
+
