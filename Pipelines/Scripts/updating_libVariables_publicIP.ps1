66c0474dc902b3b651faca46b23f5077be048349
Kolli Chaitanya <venkatachaitanya095@gmail.com>
2024-06-26 13:14:18 +0530

    PowerShell changes to update library variables #17

--- Pipelines/Scripts/updating_libVariables_publicIP.ps1
+++ Pipelines/Scripts/updating_libVariables_publicIP.ps1
@@ -1,16 +1,19 @@
 Param(
     [string]$organizationName,  # Your Azure DevOps organization name
     [string]$projectName,       # Your project name
-    [string]$libVarGroupId,     # The ID of the variable group
     [string]$libVariableName,   # The name of the variable to update
     [string]$patToken,           # Personal Access Token (PAT)
     [string]$publicIpValue
 )
 # Base64 encode the PAT
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$patToken"))
-# Get the existing variable group
-$uri = "https://dev.azure.com/$organizationName/$projectName/_apis/distributedtask/variablegroups/$libVarGroupId"+"?api-version=6.0-preview.2"
 try {
+    # API call to get the id of Library Variable
+    $libvariableuri = "https://dev.azure.com/$organizationName/$projectName/_apis/distributedtask/variablegroups/?api-version=6.0-preview.2"
+    $response = Invoke-RestMethod -Uri $libvariableuri -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
+    $libVarGroupId = ($response.value | Where-Object { $_.name -eq $libVariableName }).id
+    # Get the existing variable group
+    $uri = "https://dev.azure.com/$organizationName/$projectName/_apis/distributedtask/variablegroups/$libVarGroupId"+"?api-version=6.0-preview.2"
     $response = Invoke-RestMethod -Uri $uri -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
     # Update the variable value
     $response.variables.$libVariableName.value = $publicIpValue
