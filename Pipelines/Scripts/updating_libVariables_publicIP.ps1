94662a02ea5559840533673d76de23b1b3e8d769
Kolli Chaitanya <venkatachaitanya095@gmail.com>
2024-06-26 17:46:44 +0530

    Updated lib variable script

--- Pipelines/Scripts/updating_libVariables_publicIP.ps1
+++ Pipelines/Scripts/updating_libVariables_publicIP.ps1
@@ -4,7 +4,7 @@ Param(
     [string]$libVariableName,           # The name of the library 
     [string]$libVariableParameterName,  # The name of the variable to update
     [string]$patToken,                  # Personal Access Token (PAT)
-    [string]$value
+    [string]$valuetoUpdate
 )
 # Base64 encode the PAT
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$patToken"))
@@ -17,7 +17,7 @@ try {
     $uri = "https://dev.azure.com/$organizationName/$projectName/_apis/distributedtask/variablegroups/$libVarGroupId"+"?api-version=6.0-preview.2"
     $response = Invoke-RestMethod -Uri $uri -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
     # Update the variable value
-    $response.variables.$libVariableParameterName.value = $value
+    $response.variables.$libVariableParameterName.value = $valuetoUpdate
     # Update the variable group
     $json = $response | ConvertTo-Json -Depth 100
     Invoke-RestMethod -Uri $uri -Method Put -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo); "Content-Type"="application/json"} -Body $json
