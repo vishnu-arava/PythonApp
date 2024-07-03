54309a0cdf39252baa3c6f3edd92973311fa462c
Kolli Chaitanya <venkatachaitanya095@gmail.com>
2024-07-03 12:00:47 +0530

    made some changes to powershell and added azcli commands

--- Pipelines/Scripts/Azcli/publicip.ps1
+++ Pipelines/Scripts/Azcli/publicip.ps1
@@ -6,3 +6,4 @@ param(
 $publicIp=az vm show -d -g $rgGroup -n $vmName --query "publicIps" -o tsv
 Write-Host "##vso[task.setvariable variable=varPublicIp;]$publicIp"
 Write-Host "PublicIP is : $varpublicIP"
+Write-Host "Fetchec PublcIP is : $publicIp"
