c995d130d47e279dafde9cc352b069cf81c58322
Kolli Chaitanya <venkatachaitanya095@gmail.com>
2024-07-04 18:07:16 +0530

    Modified shellscript to update pipeline variables as well with library variables and creating a sshh service connection

--- Pipelines/Scripts/Powershell/create_serviceConnection.ps1
+++ Pipelines/Scripts/Powershell/create_serviceConnection.ps1
@@ -6,13 +6,57 @@ Param(
     [string]$userName,
     [string]$password,
     [string]$libVariableName,
-    [string]$libVariableParameterName
+    [string]$libVariableParameterName,
+    [string]$pipelineName,
+    [string]$pipelineVariableName
 )
 # Encode PAT for authorization
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($patToken)"))
 # Define the URL to fetch the project details
 $projectApiUrl = "https://dev.azure.com/$organizationName/_apis/projects/$projectName"+"?api-version=6.0"
 $serviceConnectionName = "vmServiceConnection($servicePublicIp)"
+function pipelineVariablesUpdate {
+    $pipelineurl = "https://vsrm.dev.azure.com/$organizationName/$projectName/_apis/release/releases/?api-version=7.1-preview.8"
+
+    $pipelinelist = Invoke-RestMethod -Uri $pipelineurl -Method Get -Headers @{ Authorization = "Basic $base64AuthInfo" }
+    $arr = @()
+    foreach ($pipelinelist in $pipelinelist.value) {
+        if ($pipelinelist.releaseDefinition.name -eq 'deploying_weatherapp') {
+            [int]$id = $pipelinelist.id
+            $arr += $id
+        }
+    }
+    if ($arr.Count -eq 0) {
+        Write-Error "No releases found with the name 'deploying_weatherapp'"
+        return
+    }
+    $currentReleaseId = $arr[0]
+    Write-Output "Release ID: $currentReleaseId"
+    $currentReleaseResponse = "https://vsrm.dev.azure.com/$organizationName/$projectName/_apis/release/releases/$currentReleaseId/?api-version=7.1-preview.8"
+    Write-Output "URI for fetching release details: $currentReleaseResponse"
+    $currentRelease = Invoke-RestMethod -Uri $currentReleaseResponse -Method Get -Headers @{
+        Authorization = "Basic $base64AuthInfo"
+        Accept = "application/json; api-version=7.1-preview.8"
+    }
+    $keepForever = $currentRelease.keepForever
+    Write-Output "Current keepForever value: $keepForever"
+    $currentRelease.variables.$pipelineVariableName.value = $valuetoUpdate
+    $variableBody = $currentRelease | ConvertTo-Json -Depth 100
+    # Write-Output "Variable Body: $variableBody"
+    try {
+        $updateReponse = Invoke-RestMethod -Uri $currentReleaseResponse -Method Put -Headers @{
+            Authorization = "Basic $base64AuthInfo"
+            "Content-Type" = "application/json"
+            Accept = "application/json; api-version=7.1-preview.8"
+        } -Body $variableBody
+        Write-Host $updateReponse.variable
+        Write-Host "Pipeline Variable: $pipelineVariableName updated successfully with value: $valuetoUpdate"
+    }
+    catch {
+        Write-Host "⚠️ Error updating pipeline variable: $($_.Exception.Message)"
+        exit 1
+    }
+}
 try {
     # Fetch the project details
     $projectResponse = Invoke-RestMethod -Uri $projectApiUrl -Method Get -Headers @{
@@ -72,15 +116,10 @@ try {
     $json = $response | ConvertTo-Json -Depth 100
     Invoke-RestMethod -Uri $libvariableuri -Method Put -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo); "Content-Type"="application/json"} -Body $json
     Write-Host "Variable group updated successfully with service connection: $serviceConnectionName"
+    pipelineVariablesUpdate
     exit 0
 }
 catch {
-    Write-Host "Organization Name: $organizationName"
-    Write-Host "Project Name: $projectName"
-    Write-Host "Service Connection Name: $serviceConnectionName"
-    Write-Host "Service Public IP: $servicePublicIp"
-    Write-Host "Username: $userName"
-    Write-Host "Password: $password"
-    Write-Host "Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+    Write-Host "⚠️ Error: $($_.Exception.Message)"
     exit 1
 }
