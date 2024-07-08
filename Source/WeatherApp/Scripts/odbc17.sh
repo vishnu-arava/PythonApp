a752a6ff2f9cc35a9913b62f2252268d650b8fe9
azurelaxmi08 <azurelaxmi08@gmail.com>
2024-07-08 14:17:45 +0000

    Updated odbc17.sh

--- Source/WeatherApp/Scripts/odbc17.sh
+++ Source/WeatherApp/Scripts/odbc17.sh
@@ -9,20 +9,20 @@ if ! [[ "16.04 18.04 20.04 22.04" == *"$(lsb_release -rs)"* ]]; then
 fi
 
 # Add Microsoft package repositories
-curl https://packages.microsoft.com/keys/microsoft.asc |  tee /etc/apt/trusted.gpg.d/microsoft.asc
+curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc
 
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list |  tee /etc/apt/sources.list.d/mssql-release.list
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | tee /etc/apt/sources.list.d/mssql-release.list
 
 # Update package lists and install ODBC drivers and tools
- apt-get update
- ACCEPT_EULA=Y apt-get install -y msodbcsql17
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17
 
 # Optional: for bcp and sqlcmd
- ACCEPT_EULA=Y apt-get install -y mssql-tools
+ACCEPT_EULA=Y apt-get install -y mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
 
 # Optional: for unixODBC development headers
- apt-get install -y unixodbc-dev
+apt-get install -y unixodbc-dev
 
 echo "Finished odbc17.sh script."
