be12fd37f18f290af2d873cbe565a86080b331fa
Kolli Chaitanya <venkatachaitanya095@gmail.com>
2024-07-12 19:41:41 +0530

    Added app service arm template and azcli script to create app reg and update kv

--- Pipelines/Scripts/Azcli/appRegistration.ps1
+++ Pipelines/Scripts/Azcli/appRegistration.ps1
@@ -1,27 +1,32 @@
-[string]$appRegistrationName='weatherapp-keyvault'
-[string]$appRegistrationClientSecretName='weatherapp-keyvault-client-secret'
-[string]$keyVaultName='dkweatherapp-kv-dev'
-[string]$resourceGroupName='RG-DEV'
+param(
+    [string]$appRegistrationName,
+    [string]$appRegistrationClientSecretName,
+    [string]$keyVaultName,
+    [string]$resourceGroupName,
+    [string]$DataBaseName,
+    [string]$DataBasePassword,
+    [string]$DataBaseServerLink,
+    [string]$DataBaseUserName
+)
 $tenantId=az account list --query "[?contains(user.name, 'kollichaitanya2024@gmail.com')].tenantId" --output tsv
 $appId=az ad app create --display-name $appRegistrationName --query appId --output tsv
 $objectId=az ad app show --id $appId --query id --output tsv
+$appRegServicePrincipleObjectId=az ad sp create --id $appId --query id --output tsv
+Write-Host "App service and service principle created successfully"
 $clientSecretValue=az ad app credential reset --id $objectId --append --display-name $appRegistrationClientSecretName --end-date '2024-12-31' --query password --output tsv
 
 [string]$subscriptionId=az account list --query "[?contains(user.name, 'kollichaitanya2024@gmail.com')].id" --output tsv
 
-# [string]$serviceConnectionOjectid=az ad app list --query "[?contains(displayName, 'PythonProjects-service-connection')].id" --output tsv
-
 [string]$scope = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.KeyVault/vaults/$keyVaultName"
-az role assignment create --role "Key Vault Secrets User" --assignee-object-id $objectId --scope $scope   
+az role assignment create --role "Key Vault Secrets User" --assignee-object-id $appRegServicePrincipleObjectId --scope $scope --assignee-principal-type ServicePrincipal
 
 az keyvault secret set --vault-name $keyVaultName --name "AppRegistrationClientSecret" --value $clientSecretValue
 az keyvault secret set --vault-name $keyVaultName --name "AppRegistrationTenantId" --value $tenantId
 az keyvault secret set --vault-name $keyVaultName --name "AppRegistrationClientId" --value $appId
 
-Write-Host "Client Secret is : ",$clientSecretValue
-Write-Host "Tenant id is : ",$tenantId
-Write-Host "Cliet id is : ",$appId
+az keyvault secret set --vault-name $keyVaultName --name "DataBaseName" --value $DataBaseName
+az keyvault secret set --vault-name $keyVaultName --name "DataBasePassword" --value $  
+az keyvault secret set --vault-name $keyVaultName --name "DataBaseServerLink" --value $DataBaseServerLink
+az keyvault secret set --vault-name $keyVaultName --name "DataBaseUserName" --value $DataBaseUserName
 
-
-
-#az role assignment create --role "Key Vault Secrets User" --assignee-object-id 33a4fb2f-e88b-4252-9d14-6b2d76d555be --assignee-principal-type ServicePrincipal --scope $scope
+Write-Host "Key Vault values updated"
