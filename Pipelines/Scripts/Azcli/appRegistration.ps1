7c548547dfaa147c2952af5095c9eeefdc4965de
Kolli Chaitanya <venkatachaitanya095@gmail.com>
2024-07-13 20:43:31 +0530

    azcli changes

--- Pipelines/Scripts/Azcli/appRegistration.ps1
+++ Pipelines/Scripts/Azcli/appRegistration.ps1
@@ -12,12 +12,14 @@ Write-Host "appRegServicePrincipleObjectId is : $appRegServicePrincipleObjectId"
 Write-Host "App service and service principle created successfully"
 $clientSecretValue=az ad app credential reset --id $objectId --append --display-name $appRegistrationClientSecretName --end-date '2024-12-31' --query password --output tsv
 
-[string]$subscriptionId=az account list --query "[?contains(user.name, 'kollichaitanya2024@gmail.com')].id" --output tsv
+#[string]$subscriptionId=az account list --query "[?contains(user.name, 'kollichaitanya2024@gmail.com')].id" --output tsv
+[string]$subscriptionId="2dc383a6-eed6-4aa2-a955-b4fffa7da912"
 Write-Host "Subbscription id is : $subscriptionId"
 [string]$scope = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.KeyVault/vaults/$keyVaultName"
 Write-Host "Scope is : $scope"
 az role assignment create --role "Key Vault Secrets User" --assignee-object-id $appRegServicePrincipleObjectId --scope $scope --assignee-principal-type ServicePrincipal
 
+az keyvault secret list --vault-name $keyVaultName
 az keyvault secret set --vault-name $keyVaultName --name "AppRegistrationClientSecret" --value $clientSecretValue
 az keyvault secret set --vault-name $keyVaultName --name "AppRegistrationTenantId" --value $tenantId
 az keyvault secret set --vault-name $keyVaultName --name "AppRegistrationClientId" --value $appId
