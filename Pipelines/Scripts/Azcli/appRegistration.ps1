f4fd5b2372615b7ad7fa288265ae642aab0660b6
Kolli Chaitanya <venkatachaitanya095@gmail.com>
2024-07-15 16:22:48 +0530

    Updated Azcli

--- Pipelines/Scripts/Azcli/appRegistration.ps1
+++ Pipelines/Scripts/Azcli/appRegistration.ps1
@@ -4,13 +4,13 @@ param(
     [string]$keyVaultName,
     [string]$resourceGroupName
 )
-$tenantId=az account list --query "[?contains(user.name, 'kollichaitanya2024@gmail.com')].tenantId" --output tsv
-$appId=az ad app create --display-name $appRegistrationName --query appId --output tsv
-$objectId=az ad app show --id $appId --query id --output tsv
-$appRegServicePrincipleObjectId=az ad sp create --id $appId --query id --output tsv
+[string]$tenantId="a576b579-5332-4070-ac30-3f4d5a911ac0"
+[string]$appId=az ad app create --display-name $appRegistrationName --query appId --output tsv
+[string]$objectId=az ad app show --id $appId --query id --output tsv
+[string]$appRegServicePrincipleObjectId=az ad sp create --id $appId --query id --output tsv
 Write-Host "appRegServicePrincipleObjectId is : $appRegServicePrincipleObjectId"
 Write-Host "App service and service principle created successfully"
-$clientSecretValue=az ad app credential reset --id $objectId --append --display-name $appRegistrationClientSecretName --end-date '2024-12-31' --query password --output tsv
+[string]$clientSecretValue=az ad app credential reset --id $objectId --append --display-name $appRegistrationClientSecretName --end-date '2024-12-31' --query password --output tsv
 
 #[string]$subscriptionId=az account list --query "[?contains(user.name, 'kollichaitanya2024@gmail.com')].id" --output tsv
 [string]$subscriptionId="2dc383a6-eed6-4aa2-a955-b4fffa7da912"
