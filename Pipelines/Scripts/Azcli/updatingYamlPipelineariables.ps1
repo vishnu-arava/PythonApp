5fd62104b68014fc3a72d4f261314b426550610a
Kolli Chaitanya <venkatachaitanya095@gmail.com>
2024-07-16 16:34:22 +0530

    Updated azcli powershell script to update Yaml variables

--- Pipelines/Scripts/Azcli/updatingYamlPipelineariables.ps1
+++ Pipelines/Scripts/Azcli/updatingYamlPipelineariables.ps1
@@ -7,22 +7,6 @@ Param(
     [string]$patToken
 )
 
-# Function to check if Azure DevOps CLI is installed and install it if necessary
-function Install-AzDevOpsCLI {
-    Write-Host "Checking if Azure DevOps CLI is installed..."
-    if (-not (Get-Command "az" -ErrorAction SilentlyContinue)) {
-        Write-Host "Azure DevOps CLI not found. Installing..."
-        Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://aka.ms/installazurecliwindows')
-    }
-
-    # Check if azure-devops extension is installed
-    $extensions = az extension list -o table --query "[?name=='azure-devops'].name | [0]"
-    if (-not $extensions) {
-        Write-Host "Installing azure-devops extension..."
-        az extension add --name azure-devops
-    }
-}
-
 # Function to authenticate to Azure DevOps
 function Authenticate {
     Write-Host "Authenticating to Azure DevOps..."
@@ -39,7 +23,6 @@ function Update-PipelineVariable {
 
 # Main script execution
 try {
-    Install-AzDevOpsCLI
     Authenticate
     Update-PipelineVariable
 } catch {
