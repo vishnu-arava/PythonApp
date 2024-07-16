a8c61c244294d3513f287b1d76b9b873eb9d3c1d
Kolli Chaitanya <venkatachaitanya095@gmail.com>
2024-07-16 16:23:34 +0530

    Updated azcli powershell script to update Yaml variables

--- Pipelines/Scripts/Azcli/updatingYamlPipelineariables.ps1
+++ Pipelines/Scripts/Azcli/updatingYamlPipelineariables.ps1
@@ -7,6 +7,14 @@ Param(
     [string]$patToken
 )
 
+function Install-AzDevOpsCLI {
+    Write-Host "Checking if Azure DevOps CLI is installed..."
+    if (-not (Get-Command "az" -ErrorAction SilentlyContinue)) {
+        Write-Host "Azure DevOps CLI not found. Installing..."
+        Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://aka.ms/installazurecliwindows')
+        az extension add --name azure-devops
+    }
+}
 function Authenticate {
     Write-Host "Authenticating to Azure DevOps..."
     az devops configure --defaults organization=https://dev.azure.com/$organizationName project=$projectName
@@ -20,6 +28,6 @@ function Update-PipelineVariable {
     Write-Host "Pipeline variable updated successfully."
 }
 
-
+Install-AzDevOpsCLI
 Authenticate
 Update-PipelineVariable
