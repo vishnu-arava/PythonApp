bdb456dcadb22bb178b568d4f08e1dda524c1f66
Kolli Chaitanya <venkatachaitanya095@gmail.com>
2024-07-27 12:58:33 +0530

    Updated az cli powershell sript for checking of app registration created or not #24

--- Pipelines/Scripts/Azcli/appRegistration.ps1
+++ Pipelines/Scripts/Azcli/appRegistration.ps1
@@ -11,18 +11,25 @@ param(
     [string]$dbUserName,
     [string]$patPowershell
 )
-[string]$appId=az ad app create --display-name $appRegistrationName --query appId --output tsv
+
+if(az ad app list --display-name $appRegistrationName --output tsv){
+    [string]$appId=az ad app list --display-name $appRegistrationName --query "[].appId" --output tsv
+}
+else {
+    [string]$appId=az ad app create --display-name $appRegistrationName --query appId --output tsv
+}
 [string]$objectId=az ad app show --id $appId --query id --output tsv
-[string]$appRegServicePrincipleObjectId=az ad sp create --id $appId --query id --output tsv
+if (az ad sp list --display-name $appRegistrationName --output tsv) {
+    [string]$appRegServicePrincipleObjectId=az ad sp list --display-name $appRegistrationName --query "[].id" --output tsv
+}
+else {
+    [string]$appRegServicePrincipleObjectId=az ad sp create --id $appId --query id --output tsv
+}
 Write-Host "App service and service principle created successfully"
 [string]$clientSecretValue=az ad app credential reset --id $objectId --append --display-name $appRegistrationClientSecretName --end-date '2024-12-31' --query password --output tsv
 
-[string]$scope = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.KeyVault/vaults/$keyVaultName"
-az role assignment create --role "Key Vault Secrets User" --assignee-object-id $appRegServicePrincipleObjectId --scope $scope --assignee-principal-type ServicePrincipal
- 
 az keyvault set-policy --name $keyVaultName --object-id $appRegServicePrincipleObjectId --secret-permissions get, list
 
-az keyvault secret list --vault-name $keyVaultName
 az keyvault secret set --vault-name $keyVaultName --name "AppRegistrationClientSecret" --value $clientSecretValue
 az keyvault secret set --vault-name $keyVaultName --name "AppRegistrationTenantId" --value $tenantId
 az keyvault secret set --vault-name $keyVaultName --name "AppRegistrationClientId" --value $appId
