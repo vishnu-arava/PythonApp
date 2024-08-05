a792dcee4b12281b0478116bf909ccd719fb0804
SD <admin@devopskernel.com>
2024-08-05 12:53:26 +0000

    Updated function_app.py

--- Source/FunctionApps/servicebusQueueTrigger/function_app.py
+++ Source/FunctionApps/servicebusQueueTrigger/function_app.py
@@ -3,7 +3,7 @@ import logging
 
 app = func.FunctionApp()
 
-@app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="weatherappqueue",connection="weatherappsbdev_SERVICEBUS") 
-def servicebus_queue_trigger(azservicebus: func.ServiceBusMessage):
+@app.servicebusQueueTrigger(arg_name="azservicebus", queue_name="weatherappqueue",connection="weatherappsbdev_SERVICEBUS") 
+def servicebusQueueTrigger(azservicebus: func.ServiceBusMessage):
     logging.info('Python ServiceBus Queue trigger processed a message: %s',azservicebus.get_body().decode('utf-8'))
     print('Python ServiceBus Queue trigger processed a message: %s',azservicebus.get_body().decode('utf-8'))
