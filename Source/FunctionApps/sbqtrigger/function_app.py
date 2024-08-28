0c7f844fbddba4769ecfacde2197f9e0893332a1
Satyam Dasari <mybuildserver@gmail.com>
2024-08-28 17:06:43 +0530

    added new func

---
+++ Source/FunctionApps/sbqtrigger/function_app.py
@@ -0,0 +1,10 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp()
+
+@app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="weatherappqueue",
+                               connection="weatherappsbvc_SERVICEBUS") 
+def servicebus_queue_trigger(azservicebus: func.ServiceBusMessage):
+    logging.info('Python ServiceBus Queue trigger processed a message: %s',
+                azservicebus.get_body().decode('utf-8'))
